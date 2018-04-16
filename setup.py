311723e2b4a6e257777503ce59e06af0d5d7f1fd
asbl <andreas.siebelqit-teaching.de>
2018-04-16 06:17:42 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'gamegridp',
   packages = ['gamegridp'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.11',
   description = 'A gamegrid clone for python',
   author = 'Andreas Siebel',
   author_email = 'andreas.siebel@it-teaching.de',
